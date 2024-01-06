890fe3b730cd4c43fe803907006636ba367f952a
Raghuram <raghuram.kothakota@gmail.com>
2024-01-05 21:54:14 -0800

    Restructure after first auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -24,13 +24,11 @@ public class Robot {
 
     public DcMotorEx motorSlider; // Slider
     public Servo servoArm; // Elbow or Arm
-    public DcMotorEx motorHang; // Hanging
 
     public Servo servoCR; // Claw Right
     public Servo servoCL; // Claw left
     public Servo servoDrone; // Drone
     public Telemetry telemetry;
-    public BHI260IMU imu;
 
     private VisionPortal visionPortal;
     private AprilTagProcessor aprilTag;
@@ -43,12 +41,8 @@ public class Robot {
         motorFR = hardwareMap.get(DcMotor.class, "front_right");
         motorBL = hardwareMap.get(DcMotor.class, "back_left");
         motorBR = hardwareMap.get(DcMotor.class, "back_right");
-        // change the direction for the FR and BR so that all positive direction is forward
-        //motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
-        //motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
         motorSlider = hardwareMap.get(DcMotorEx.class, "sliders");
         servoArm = hardwareMap.get(Servo.class, "arm");
-        motorHang = hardwareMap.get(DcMotorEx.class, "hanging");
         servoCL = hardwareMap.get(Servo.class, "claw_left");
         servoCR = hardwareMap.get(Servo.class, "claw_right");
         servoDrone = hardwareMap.get(Servo.class, "drone");
@@ -56,32 +50,6 @@ public class Robot {
         sampleDrive = new SampleMecanumDrive(hardwareMap);
 
         //servoArm.setPosition(0.2);
-
-        /*
-        imu = hardwareMap.get(BHI260IMU.class, "imu");
-        imu.initialize(
-            new IMU.Parameters(
-                new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
-                )
-            )
-        );
-         */
-    }
-
-    public double getYaw() {
-        YawPitchRollAngles robotOrientation;
-        robotOrientation = imu.getRobotYawPitchRollAngles();
-
-        double Yaw   = robotOrientation.getYaw(AngleUnit.DEGREES);
-        return Yaw;
-        //double Pitch = robotOrientation.getPitch(AngleUnit.DEGREES);
-        //double Roll  = robotOrientation.getRoll(AngleUnit.DEGREES);
-    }
-
-    public void resetYaw() {
-        imu.resetYaw();
     }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TgeDetectorPipeline.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class TgeDetectorPipeline extends OpenCvPipeline {
+    Rect z1Rect = new Rect(20, 280, 150, 150);
+    Rect z2Rect = new Rect(220, 240, 150, 150);
+    Rect z3Rect = new Rect(420, 280, 150, 150);
+
+    Scalar red = new Scalar(255, 0, 0);
+    Scalar blue = new Scalar(0, 0, 255);
+
+    String tgeColor = "blue";
+    int tgeZone = -1;
+
+    public TgeDetectorPipeline() {
+        super();
+    }
+
+    @Override
+    public Mat processFrame(Mat frame) {
+        double blueDist[] = new double[3];
+        double redDist[] = new double[3];
+        Scalar z1AvgColor, z2AvgColor, z3AvgColor;
+        Mat z1, z2, z3;
+
+        z1 = frame.submat(z1Rect);
+        z2 = frame.submat(z2Rect);
+        z3 = frame.submat(z3Rect);
+
+        z1AvgColor = Core.mean(z1);
+        z2AvgColor = Core.mean(z2);
+        z3AvgColor = Core.mean(z3);
+
+        blueDist[0] = colorDist(z1AvgColor, blue);
+        blueDist[1] = colorDist(z2AvgColor, blue);
+        blueDist[2] = colorDist(z3AvgColor, blue);
+
+        redDist[0] = colorDist(z1AvgColor, red);
+        redDist[1] = colorDist(z2AvgColor, red);
+        redDist[2] = colorDist(z3AvgColor, red);
+
+        int bluemin = 0;
+        int redmin = 0;
+        for (int i = 1; i < 3; i++) {
+            if (blueDist[i] < blueDist[bluemin]) {
+                bluemin = i;
+            }
+            if (redDist[i] < redDist[redmin]) {
+                redmin = i;
+            }
+        }
+
+        tgeColor = "blue";
+        tgeZone = bluemin + 1;
+        if (redDist[redmin] < blueDist[bluemin]) {
+            tgeZone = redmin + 1;
+            tgeColor = "red";
+        }
+        return frame;
+    }
+
+    public double colorDist(Scalar c1, Scalar c2){
+        double rDiff = c1.val[0] - c2.val[0];
+        double gDiff = c1.val[1] - c2.val[1];
+        double bDiff = c1.val[2] - c2.val[2];
+        return Math.sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff);
+    }
+
+    public int getTgeZone() {
+        return tgeZone;
+    }
+    public String getTgeColor() {
+        return tgeColor;
+    }
+}
